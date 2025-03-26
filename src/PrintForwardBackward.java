2b1ecbef07262163a068920fdb0ff3a7aee4827f
ahazna <contact@ahazna.com>
2025-03-26 11:03:59 +0300

    printing number using recursion

---
+++ src/PrintForwardBackward.java
@@ -0,0 +1,22 @@
+public class PrintForwardBackward {
+// A Java program to demonstrate working of
+// recursion
+    static void printFun(int test)
+    {
+        if (test < 1)
+            return;
+        else {
+            System.out.printf("%d ", test);
+            printFun(test - 1); // statement 2
+            System.out.printf("%d ", test);
+            return;
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int test = 3;
+        printFun(test);
+    }
+}
